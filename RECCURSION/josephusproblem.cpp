3ccde08391bf839d29dbb0c78672ec30402e80e0
AdityaDebnath7 <adityadebnath0202@gmail.com>
2023-12-21 22:39:10 +0530

    added josephus

--- RECCURSION/josephusproblem.cpp
+++ RECCURSION/josephusproblem.cpp
@@ -1,8 +1,48 @@
 #include <iostream>
 using namespace std;
-    
-int main() {
- 
 
-     return 0 ;
+int arrkth(int n, int k)
+{
+
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        arr[i] = i;
+    }
+
+    if ((k) % (n) == 0)
+    {
+        return arr[n - 1];
+    }
+    else
+    {
+        return arr[(k % n) - 1];
+    }
+}
+
+int jos(int n, int k)
+{
+
+    if (n == 1)
+    {
+        return 0;
+    }
+    else
+        return arrkth(n, (jos(n - 1, k)+(k+1)));
+}
+
+int main()
+{
+    int n ,k;
+while(true){
+    cout<<"enter n"<<endl;
+    cin>>n;
+    cout<<"enter k"<<endl;
+    cin>>k;cout<<endl;
+    cout<<"survivor is "<<jos(n,k)<<endl;
+}
+   
+   
+
+    return 0;
 }
